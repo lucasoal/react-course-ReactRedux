78ef768f11837335223c46f696c38a5db9fa166f
lucasoal <lucasoalzst@gmail.com>
2021-12-20 17:57:04 -0300

    aula 36

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from './components/ExpenseItem'
+
+function App() {
+  return (
+    <div>
+      <h2>React And Redux</h2>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
